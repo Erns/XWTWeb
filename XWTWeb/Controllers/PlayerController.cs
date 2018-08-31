836a68593ceacf61e33807fb6a59893737812b96
aaron.sayles <aaron.sayles@gmail.com>
2018-08-30 22:23:41 -0500

    20180830

---
+++ XWTWeb/Controllers/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace XWTWeb.Controllers
+{
+    public class PlayerController : Controller
+    {
+        // GET: Player
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Home()
+        {
+            ViewBag.Message = "Your player home page.";
+
+            return View();
+        }
+    }
+}
