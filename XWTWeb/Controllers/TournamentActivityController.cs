612d11d51d6e4ba1e2443ee007576e66c2fba071
aaron.sayles <aaron.sayles@gmail.com>
2018-10-02 15:23:34 -0500

    20181002

---
+++ XWTWeb/Controllers/TournamentActivityController.cs
@@ -0,0 +1,55 @@
+﻿using Newtonsoft.Json;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using XWTWeb.Classes;
+using XWTWeb.Models;
+
+namespace XWTWeb.Controllers
+{
+    [RequireHttps]
+    [Authorize]
+    public class TournamentActivityController : Controller
+    {
+        RestClient client = Utilities.InitializeRestClient();
+
+
+        // GET: TournamentActivity
+        public ActionResult Main(int id)
+        {
+
+            TournamentMain tournament = new TournamentMain();
+
+            try
+            {
+                if (id > 0)
+                {
+                    var request = new RestRequest("Tournaments/{userid}/{id}", Method.GET);
+                    request.AddUrlSegment("userid", Utilities.CurrentUser.Id);
+                    request.AddUrlSegment("id", id);
+
+                    // execute the request
+                    IRestResponse response = client.Execute(request);
+                    var content = response.Content;
+
+                    List<TournamentMain> result = JsonConvert.DeserializeObject<List<TournamentMain>>(JsonConvert.DeserializeObject(content).ToString());
+                    foreach (TournamentMain tmpTournament in result)
+                    {
+                        tournament = tmpTournament;
+                        break;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.Write(string.Format("TournamentActivityController{0}Main:{1}", Environment.NewLine, ex.Message));
+            }
+
+            return View(tournament);
+
+        }
+    }
+}
