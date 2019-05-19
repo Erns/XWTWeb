06a443cc5cb0bb134093133ccc9145babb28e9ed
aaron.sayles <aaron.sayles@gmail.com>
2019-05-18 21:58:16 -0500

    Starting to switch to Vue

--- XWTWeb/Scripts/utilities.js
+++ XWTWeb/Scripts/utilities.js
@@ -19,11 +19,11 @@ var blnBBoxLoading = false;
 var inptFocused = null;
 function BBoxLoading() {
     blnBBoxLoading = true;
-    bootbox.dialog({ message: '<div class="text-center"><i class="fa fa-spin fa-spinner"></i> Loading...</div>', closeButton: false }).on('shown.bs.modal',
-        function () {
-            //If we've already set the close the loading box before it's finished opening, make sure it closes
-            if (!blnBBoxLoading) BBoxLoadingClose();
-        });
+    //bootbox.dialog({ message: '<div class="text-center"><i class="fa fa-spin fa-spinner"></i> Loading...</div>', closeButton: false }).on('shown.bs.modal',
+    //    function () {
+    //        //If we've already set the close the loading box before it's finished opening, make sure it closes
+    //        if (!blnBBoxLoading) BBoxLoadingClose();
+    //    });
 }
 
 function BBoxLoadingClose() {
