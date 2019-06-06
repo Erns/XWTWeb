0bccaa0d9e8ea6744d5ed55916115b9a686e0c22
Erns <aaron.sayles@gmail.com>
2019-06-06 15:44:34 -0500

    Update PlayerMainComp.js

--- XWTWeb/Scripts/Player/PlayerMainComp.js
+++ XWTWeb/Scripts/Player/PlayerMainComp.js
@@ -1,28 +1,26 @@
 const playerRow = Vue.component('player-row', {
-    props: ['data', 'canEdit'],
+    props: ['data'],
     template: 
-            `<tr readonly :id="'playerMainTableRow_' + data.Id" v-bind:data-playerid="data.Id">
-                    <td class="align-middle"><input :readonly="!canEdit" :id="'playerMainTableRowName_' + data.Id" type="text" class="required col form-control form-control-sm" v-model:value="data.Name" placeholder="Name" data-valuetype="Name" data-originalvalue="data.Name" @dblclick="unlockInpt(canEdit)"/></td>
-                    <td class="align-middle"><input readonly :id="'playerMainTableRowEmail_' + data.Id" type="text" class="col form-control form-control-sm" :value="data.Email" placeholder="Email Address" data-valuetype="Email" data-originalvalue="data.Email" ondblclick="UnlockInput(this);" ontouchstart="UnlockInput(this);" /></td>
-                    <td class="align-middle"><input readonly :id="'playerMainTableRowGroup_' + data.Id" type="text" class="col form-control form-control-sm" :value="data.Group" placeholder="Group" data-valuetype="Group" data-originalvalue="data.Group" ondblclick="UnlockInput(this);" ontouchstart="UnlockInput(this);" /></td>
+            `<tr :id="'playerMainTableRow_' + data.Id" v-bind:data-playerid="data.Id">
+                    <td class="align-middle"><input v-bind:readonly="!data.CanEdit" :id="'playerMainTableRowName_' + data.Id" type="text" class="required col form-control form-control-sm" v-model:value="data.Name" :data-original="data.OriginalName" placeholder="Name" data-valuetype="Name" v-on:dblclick="unlockInpt(data)"/></td>
+                    <td class="align-middle"><input v-bind:readonly="!data.CanEdit" :id="'playerMainTableRowEmail_' + data.Id" type="text" class="col form-control form-control-sm" v-model:value="data.Email" :data-original="data.OriginalEmail" placeholder="Email Address" data-valuetype="Email" data-originalvalue="data.Email" v-on:dblclick="unlockInpt(data)" /></td>
+                    <td class="align-middle"><input v-bind:readonly="!data.CanEdit" :id="'playerMainTableRowGroup_' + data.Id" type="text" class="col form-control form-control-sm" v-model:value="data.Group" :data-original="data.OriginalGroup" placeholder="Group" data-valuetype="Group" data-originalvalue="data.Group" v-on:dblclick="unlockInpt(data)" /></td>
                     <td class="d-flex justify-content-center align-content-center">
                         <a href="#" title="Delete" style="font-weight:bold" class="btn btn-danger fas fa-trash-alt m-0 mr-1 p-2" onclick="RemovePrompt(this);"></a>
-                        <a v-if="data.Id > 0" href="#" title="Undo" style="font-weight:bold" class="btn btn-warning fas fa-undo m-0 ml-1 p-2" :onclick="'UndoPrompt(' + data.Id + ');'"></a>
+                        <a v-if="data.Id > 0" href="#" title="Undo" style="font-weight:bold" class="btn btn-warning fas fa-undo m-0 ml-1 p-2" v-on:click="undoPrompt(data)"></a>
+                        <b-button v-b-modal.modal-1>testing</b-button>
                     </td>
             </tr > `,
     methods: {
-        unlockInpt: function (canEdit) {
-            debugger
-            canEdit = true;
-            
+        unlockInpt: function (data) {
+            data.CanEdit = true;
+            SetIsDirty(true);
         },
-    },
-    watch: {
-        canEdit: function (old, newthing) {
-            debugger
+        undoPrompt: function (data) {
+            data.CanEdit = false;
+            data.Name = data.OriginalName;
+            data.Email = data.OriginalEmail;
+            data.Group = data.OriginalGroup;
         }
     }
 });
-
-
-//ontouchstart = "UnlockInput(this);" onfocus = "$(this).popover('dispose');" 
